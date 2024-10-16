aa9de24ec596e0bd9aa1477f6a6903d34fdb0106
AuroraLantean <raymondlieu.crypto@runbox.com>
2024-10-16 17:06:34 +0300

    fix fibonacci func()

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # rust-code-challenge
 
+## Write you code
+add code to src/d0/
+add that file to src/d0/mod.rs
+
 https://leetcode.com/problems/happy-number/
 
 ## Run Tests
@@ -13,7 +17,7 @@ cargo test -- --nocapture
 To run one test:
 
 ```
-cargo test test-name -- --nocapture
+cargo test testName -- --nocapture
 ```
 
 ## Reference and Tutorial Source
--- src/d0/_67_add_binary.rs
+++ src/d0/_67_add_binary.rs
@@ -3,35 +3,35 @@
 struct Solution;
 
 impl Solution {
-    fn add_binary(a: String, b: String) -> String {
-        println!("--------== 67 Add Binary. a: {}, b: {},", a, b);
-        let aa = u128::from_str_radix(&a, 2).unwrap_or(0);
-        let bb = u128::from_str_radix(&b, 2).unwrap_or(0);
-        println!("aa: {}, bb: {},", aa, bb);
-        let sum = aa + bb;
-        if sum - aa != bb {
-            format!("{:b}", sum)+ "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
-        } else {
-            println!("no overflow");
-            println!("u128::MAX: {:?}, sum: {}", u128::MAX, sum);
-            format!("{:b}", u128::MAX);
-            format!("{sum:b}")
-        }
+  fn add_binary(a: String, b: String) -> String {
+    println!("--------== 67 Add Binary. a: {}, b: {},", a, b);
+    let aa = u128::from_str_radix(&a, 2).unwrap_or(0);
+    let bb = u128::from_str_radix(&b, 2).unwrap_or(0);
+    println!("aa: {}, bb: {},", aa, bb);
+    let sum = aa + bb;
+    if sum - aa != bb {
+      format!("{:b}", sum)+ "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+    } else {
+      println!("no overflow");
+      println!("u128::MAX: {:?}, sum: {}", u128::MAX, sum);
+      //format!("{:b}", u128::MAX);
+      format!("{sum:b}")
     }
+  }
 }
 
 #[test]
 fn test67() {
-    assert_eq!(
-        Solution::add_binary("11".to_string(), "1".to_string()),
-        "100".to_string()
-    );
-    assert_eq!(
-        Solution::add_binary("1010".to_string(), "1011".to_string()),
-        "10101".to_string()
-    );
+  assert_eq!(
+    Solution::add_binary("11".to_string(), "1".to_string()),
+    "100".to_string()
+  );
+  assert_eq!(
+    Solution::add_binary("1010".to_string(), "1011".to_string()),
+    "10101".to_string()
+  );
 
-    assert_eq!(
+  assert_eq!(
         Solution::add_binary("11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111".to_string(), "1".to_string()),
         "100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000".to_string()
     );
